b71d7990dc277e195ad724ea9be6abb6e0e1ef6e
efri_yang <948061564@qq.com>
2017-07-27 17:08:35 +0800

    家具装修专题PC端

--- src/JiaZhuang/JiaJuZhuangXiuZhuanTi/pc/js/index.js
+++ src/JiaZhuang/JiaJuZhuangXiuZhuanTi/pc/js/index.js
@@ -353,4 +353,17 @@ $(function() {
             show: true
         }
     });
+
+
+    $(".bqjs-banner").PicCarousel({
+        "width":600,       //幻灯片的宽度
+        "height":355,       //幻灯片的高度
+        "posterWidth":485,  //幻灯片第一帧的宽度
+        "posterHeight":355, //幻灯片第一张的高度
+        "scale":0.9,        //记录显示比例关系
+        "speed":300,        //记录幻灯片滚动速度
+        "autoPlay":false,    //是否开启自动播放
+        "delay":1000,       //自动播放间隔
+        "verticalAlign":"top"   //图片对齐位置
+    });
 });
