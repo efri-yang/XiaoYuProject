5e4dd7cec237a20f5591f69fe5e78075ef5f0916
efri-yang <948061564@qq.com>
2017-09-12 18:13:28 +0800

    xg

--- src/BoBingGongJu/h5xuanchuan/js/index.js
+++ src/BoBingGongJu/h5xuanchuan/js/index.js
@@ -649,41 +649,41 @@ $(function() {
         })
     })
    
-    $(window).on("load", function() {
-        var swiper = new Swiper('.swiper-container', {
-            scrollbar: '.swiper-scrollbar',
-            direction: 'vertical',
-            slidesPerView: 'auto',
-            mousewheelControl: true,
-            freeMode: true,
-            freeModeMomentumRatio: 2,
-        });
-        var anis = swiper.slides[0].querySelectorAll('.ani');
-        for (i = 0; i < anis.length; i++) {
-            anis[i].style.visibility = 'hidden';
-            anis[i].classCache = anis[i].className;
-            anis[i].className = '';
-        }
+    // $(window).on("load", function() {
+    //     var swiper = new Swiper('.swiper-container', {
+    //         scrollbar: '.swiper-scrollbar',
+    //         direction: 'vertical',
+    //         slidesPerView: 'auto',
+    //         mousewheelControl: true,
+    //         freeMode: true,
+    //         freeModeMomentumRatio: 2,
+    //     });
+    //     var anis = swiper.slides[0].querySelectorAll('.ani');
+    //     for (i = 0; i < anis.length; i++) {
+    //         anis[i].style.visibility = 'hidden';
+    //         anis[i].classCache = anis[i].className;
+    //         anis[i].className = '';
+    //     }
 
 
 
 
-        setInterval(checkScroll, 50);
+    //     setInterval(checkScroll, 50);
 
-        function checkScroll() {
+    //     function checkScroll() {
 
-            _wrapperTranslate = swiper.getWrapperTranslate()
+    //         _wrapperTranslate = swiper.getWrapperTranslate()
 
-            for (i = 0; i < anis.length; i++) {
-                if (anis[i].offsetTop < (-_wrapperTranslate + swiper.height - 50) && anis[i].style.visibility == 'hidden') {
-                    anis[i].style.visibility = 'visible';
-                    anis[i].className = "" + anis[i].classCache + " " + 'animated';
+    //         for (i = 0; i < anis.length; i++) {
+    //             if (anis[i].offsetTop < (-_wrapperTranslate + swiper.height - 50) && anis[i].style.visibility == 'hidden') {
+    //                 anis[i].style.visibility = 'visible';
+    //                 anis[i].className = "" + anis[i].classCache + " " + 'animated';
 
-                }
-            }
+    //             }
+    //         }
 
-        }
-    })
+    //     }
+    // })
 
 
 })
