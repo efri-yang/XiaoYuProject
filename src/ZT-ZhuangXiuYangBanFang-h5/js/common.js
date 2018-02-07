ca50e9e4842b9b8de1f48bed9823f3833b51aae4
efri-yang <948061564@qq.com>
2018-02-07 12:39:16 +0800

    c

--- src/ZT-ZhuangXiuYangBanFang-h5/js/common.js
+++ src/ZT-ZhuangXiuYangBanFang-h5/js/common.js
@@ -1,129 +1,169 @@
-(function($){
-  'use strict';
+(function($) {
+    'use strict';
 
-  // TAB CLASS DEFINITION
-  // ====================
+    // TAB CLASS DEFINITION
+    // ====================
 
-  var Tab = function (element) {
-    // jscs:disable requireDollarBeforejQueryAssignment
-    this.element = $(element)
-    // jscs:enable requireDollarBeforejQueryAssignment
-  }
-
-  Tab.VERSION = '3.3.7'
-
-  Tab.TRANSITION_DURATION = 150
-
-  Tab.prototype.show = function () {
-    var $this    = this.element
-    var $ul      = $this.closest('ul:not(.dropdown-menu)')
-    var selector = $this.data('target')
-
-    if (!selector) {
-      selector = $this.attr('href')
-      selector = selector && selector.replace(/.*(?=#[^\s]*$)/, '') // strip for ie7
+    var Tab = function(element) {
+        // jscs:disable requireDollarBeforejQueryAssignment
+        this.element = $(element)
+        // jscs:enable requireDollarBeforejQueryAssignment
     }
 
-    if ($this.parent('li').hasClass('active')) return
+    Tab.VERSION = '3.3.7'
 
-    var $previous = $ul.find('.active  a')
-    var hideEvent = $.Event('hidetab', {
-      relatedTarget: $this[0]
-    })
-    var showEvent = $.Event('showtab', {
-      relatedTarget: $previous[0]
-    })
+    Tab.TRANSITION_DURATION = 150
+
+    Tab.prototype.show = function() {
+        var $this = this.element
+        var $ul = $this.closest('ul:not(.dropdown-menu)')
+        var selector = $this.data('target')
+
+        if (!selector) {
+            selector = $this.attr('href')
+            selector = selector && selector.replace(/.*(?=#[^\s]*$)/, '') // strip for ie7
+        }
+
+        if ($this.parent('li').hasClass('active')) return
+
+        var $previous = $ul.find('.active  a')
+        var hideEvent = $.Event('hidetab', {
+            relatedTarget: $this[0]
+        })
+        var showEvent = $.Event('showtab', {
+            relatedTarget: $previous[0]
+        })
 
 
 
-    $previous.trigger(hideEvent)
-    $this.trigger(showEvent)
+        $previous.trigger(hideEvent)
+        $this.trigger(showEvent)
 
-    if (showEvent.isDefaultPrevented() || hideEvent.isDefaultPrevented()) return
+        if (showEvent.isDefaultPrevented() || hideEvent.isDefaultPrevented()) return
 
-    var $target = $(selector)
+        var $target = $(selector)
 
-    this.activate($this.closest('li'), $ul)
-    this.activate($target, $target.parent(), function () {
-      $previous.trigger({
-        type: 'hiddentab',
-        relatedTarget: $this[0]
-      })
-      $this.trigger({
-        type: 'showntab',
-        relatedTarget: $previous[0]
-      })
-    })
-  }
-
-  Tab.prototype.activate = function (element, container, callback) {
-    var $active    = container.find('> .active')
-    var transition = callback
-      && $.support.transition
-      && ($active.length && $active.hasClass('fade') || !!container.find('> .fade').length)
-
-    function next() {
-      $active
-        .removeClass('active')
-        .find('> .dropdown-menu > .active')
-          .removeClass('active')
-        .end()
-        .find('[data-toggle="tab"]')
-          .attr('aria-expanded', false)
-
-      element
-        .addClass('active')
-        .find('[data-toggle="tab"]')
-          .attr('aria-expanded', true)
-
-      if (transition) {
-        element[0].offsetWidth // reflow for transition
-        element.addClass('in')
-      } else {
-        element.removeClass('fade')
-      }
-
-      if (element.parent('.dropdown-menu').length) {
-        element
-          .closest('li.dropdown')
-            .addClass('active')
-          .end()
-          .find('[data-toggle="tab"]')
-            .attr('aria-expanded', true)
-      }
-
-      callback && callback()
+        this.activate($this.closest('li'), $ul)
+        this.activate($target, $target.parent(), function() {
+            $previous.trigger({
+                type: 'hiddentab',
+                relatedTarget: $this[0]
+            })
+            $this.trigger({
+                type: 'showntab',
+                relatedTarget: $previous[0]
+            })
+        })
     }
 
-    $active.length && transition ?
-      $active
-        .one('bsTransitionEnd', next)
-        .emulateTransitionEnd(Tab.TRANSITION_DURATION) :
-      next()
+    Tab.prototype.activate = function(element, container, callback) {
+        var $active = container.find('> .active')
+        var transition = callback &&
+            $.support.transition &&
+            ($active.length && $active.hasClass('fade') || !!container.find('> .fade').length)
 
-    $active.removeClass('in')
-  }
+        function next() {
+            $active
+                .removeClass('active')
+                .find('> .dropdown-menu > .active')
+                .removeClass('active')
+                .end()
+                .find('[data-toggle="tab"]')
+                .attr('aria-expanded', false)
+
+            element
+                .addClass('active')
+                .find('[data-toggle="tab"]')
+                .attr('aria-expanded', true)
+
+            if (transition) {
+                element[0].offsetWidth // reflow for transition
+                element.addClass('in')
+            } else {
+                element.removeClass('fade')
+            }
+
+            if (element.parent('.dropdown-menu').length) {
+                element
+                    .closest('li.dropdown')
+                    .addClass('active')
+                    .end()
+                    .find('[data-toggle="tab"]')
+                    .attr('aria-expanded', true)
+            }
+
+            callback && callback()
+        }
+
+        $active.length && transition ?
+            $active
+            .one('bsTransitionEnd', next)
+            .emulateTransitionEnd(Tab.TRANSITION_DURATION) :
+            next()
+
+        $active.removeClass('in')
+    }
 
 
-  // TAB PLUGIN DEFINITION
-  // =====================
+    // TAB PLUGIN DEFINITION
+    // =====================
 
-  function Plugin(option) {
-    return this.each(function () {
-      var $this = $(this)
-      var data  = $this.data('bs.tab')
+    function Plugin(option) {
+        return this.each(function() {
+            var $this = $(this)
+            var data = $this.data('bs.tab')
 
-      if (!data) $this.data('bs.tab', (data = new Tab(this)))
-      if (typeof option == 'string') data[option]()
-    })
-  }
+            if (!data) $this.data('bs.tab', (data = new Tab(this)))
+            if (typeof option == 'string') data[option]()
+        })
+    }
 
-  var old = $.fn.tab
+    var old = $.fn.tab
 
-  $.fn.tab= Plugin
-  $.fn.tab.Constructor = Tab
+    $.fn.tab = Plugin
+    $.fn.tab.Constructor = Tab
 
 
-  // TAB NO CONFLICT
-  // ===============
+    // TAB NO CONFLICT
+    // ===============
+})(Zepto);
+
+
+(function($) {
+    $(document).on("mouseover", '[data-roler="dropdown"]', function() {
+        $(this).addClass("active");
+    }).on("mouseout", '[data-roler="dropdown"]', function(event) {
+        var relateElem = event.relatedTarget;
+        $(this).removeClass("active");
+        if ($(relateElem).closest($(this)).length > 0) {
+            return;
+        } else {
+            $(this).children("ul").hide();
+        }
+    });
+    $(document).on("click", '[data-roler="dropdown"] > cite', function() {
+        var $this = $(this);
+        $this.siblings("ul").show();
+    });
+    $(document).on("click", '[data-roler="dropdown"] > ul li a', function(event) {
+        event.preventDefault();
+        var $this = $(this);
+        dropdownHandle($this);
+    });
+    var $dropDown = $('#J_zsjs-order-dialog [data-roler="dropdown"]');
+    var $cite = $dropDown.children('cite');
+    var $ul = $dropDown.children('ul');
+    var $hiddenInut = $("#J_dropselhidden");
+    if (!!$hiddenInut.val()) $cite.removeClass("active");
+    /**
+     * [hiddenInut description]
+     * $elem 表示当前点击的那个元素(开发修改写在这里)
+     */
+    function dropdownHandle($elem) {
+        var selId = $elem.attr("selid");
+        var selText = $elem.text();
+        $hiddenInut.val(selId);
+        $cite.addClass('active').text(selText);
+        $ul.hide();
+    }
 })(Zepto);
