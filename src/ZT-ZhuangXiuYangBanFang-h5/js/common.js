9f53c37536e9e5ef4723cfcfcf3aa90d818cde40
efri-yang <948061564@qq.com>
2018-02-07 14:20:20 +0800

    c

--- src/ZT-ZhuangXiuYangBanFang-h5/js/common.js
+++ src/ZT-ZhuangXiuYangBanFang-h5/js/common.js
@@ -130,40 +130,43 @@
 
 
 (function($) {
-    $(document).on("mouseover", '[data-roler="dropdown"]', function() {
-        $(this).addClass("active");
-    }).on("mouseout", '[data-roler="dropdown"]', function(event) {
-        var relateElem = event.relatedTarget;
-        $(this).removeClass("active");
-        if ($(relateElem).closest($(this)).length > 0) {
-            return;
-        } else {
-            $(this).children("ul").hide();
+
+    $(function() {
+        $(document).on("click", function(event) {
+            var relateElem = event.relatedTarget;
+            $(this).removeClass("active");
+            if ($(relateElem).closest($('[data-roler="dropdown"]')).length > 0) {
+                return;
+            } else {
+                $('[data-roler="dropdown"]').children("ul").hide();
+            }
+        });
+        $(document).on("click", '[data-roler="dropdown"] cite', function() {
+            var $this = $(this);
+            $this.siblings("ul").show();
+        });
+        $(document).on("click", '[data-roler="dropdown"] ul li a', function(event) {
+            event.preventDefault();
+            var $this = $(this);
+            dropdownHandle($this);
+        });
+        var $dropDown = $('#J_dialog-bm-box [data-roler="dropdown"]');
+        var $cite = $dropDown.children('cite');
+        var $ul = $dropDown.children('ul');
+        var $hiddenInut = $("#J_dropselhidden");
+        if (!!$hiddenInut.val()) $cite.removeClass("active");
+        /**
+         * [hiddenInut description]
+         * $elem 表示当前点击的那个元素(开发修改写在这里)
+         */
+        function dropdownHandle($elem) {
+            var selId = $elem.attr("selid");
+            var selText = $elem.text();
+            console.dir($dropDown.length)
+            $hiddenInut.val(selId);
+            $cite.addClass('active').text(selText);
+            $ul.hide();
+
         }
-    });
-    $(document).on("click", '[data-roler="dropdown"] > cite', function() {
-        var $this = $(this);
-        $this.siblings("ul").show();
-    });
-    $(document).on("click", '[data-roler="dropdown"] > ul li a', function(event) {
-        event.preventDefault();
-        var $this = $(this);
-        dropdownHandle($this);
-    });
-    var $dropDown = $('#J_zsjs-order-dialog [data-roler="dropdown"]');
-    var $cite = $dropDown.children('cite');
-    var $ul = $dropDown.children('ul');
-    var $hiddenInut = $("#J_dropselhidden");
-    if (!!$hiddenInut.val()) $cite.removeClass("active");
-    /**
-     * [hiddenInut description]
-     * $elem 表示当前点击的那个元素(开发修改写在这里)
-     */
-    function dropdownHandle($elem) {
-        var selId = $elem.attr("selid");
-        var selText = $elem.text();
-        $hiddenInut.val(selId);
-        $cite.addClass('active').text(selText);
-        $ul.hide();
-    }
+    })
 })(Zepto);
