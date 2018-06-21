40c53d6679b46bf3ef1a0f64fd498421b913b032
efri-yang <948061564@qq.com>
2018-06-21 17:39:52 +0800

    cc

--- src/ZT-JiaJuYouXuan/pc/js/index.js
+++ src/ZT-JiaJuYouXuan/pc/js/index.js
@@ -40,5 +40,98 @@
 
         });
 
+
+
+        $("#J_lqwy-btn-1,#J_lqwy-btn-2").on("click",function(event){
+            event.preventDefault();
+            layer.open({
+              type: 1,
+              shadeClose: true,
+              title: false, //不显示标题
+              area:"450px",
+              content: $('#J_dialog-lqwj'), //捕获的元素，注意：最好该指定的元素要存放在body最外层，否则可能被其它的相对元素所影响
+              cancel: function(){
+                // layer.msg('捕获就是从页面已经存在的元素上，包裹layer的结构', {time: 5000, icon:6});
+              }
+            });
+        });
+
+        $("#J_lqyhj-btn").on("click",function(event){
+            event.preventDefault();
+            layer.open({
+              type: 1,
+              shadeClose: true,
+              title: false, //不显示标题
+              area:"450px",
+              content: $('#J_dialog-lqyhj'), //捕获的元素，注意：最好该指定的元素要存放在body最外层，否则可能被其它的相对元素所影响
+              cancel: function(){
+                // layer.msg('捕获就是从页面已经存在的元素上，包裹layer的结构', {time: 5000, icon:6});
+              }
+            });
+        });
+
+
+        $("#J_aboutus").on("click",function(event){
+            event.preventDefault();
+            layer.open({
+              type: 1,
+              shadeClose: true,
+              title: false, //不显示标题
+              area:"450px",
+              content: $('#J_dialog-aboutus'), //捕获的元素，注意：最好该指定的元素要存放在body最外层，否则可能被其它的相对元素所影响
+              cancel: function(){
+                // layer.msg('捕获就是从页面已经存在的元素上，包裹layer的结构', {time: 5000, icon:6});
+              }
+            });
+        });
+
+
+        $(".mod-3 .mfsw-btn").on("click",function(event){
+             event.preventDefault();
+             layer.open({
+              type: 1,
+              shadeClose: true,
+              title: false, //不显示标题
+              area:"450px",
+              content: $('#J_dialog-yhj'), //捕获的元素，注意：最好该指定的元素要存放在body最外层，否则可能被其它的相对元素所影响
+              cancel: function(){
+                  
+              }
+            });
+        });
+
+        //预约成功以后弹出框，开发根据需要嵌入
+        layer.open({
+              type: 1,
+              shadeClose: true,
+              title: false, //不显示标题
+              area:"450px",
+              content: $('#J_dialog-result'), //捕获的元素，注意：最好该指定的元素要存放在body最外层，否则可能被其它的相对元素所影响
+              cancel: function(){
+                  
+              }
+         });
+
+
+       $("#J_side-nav-1").on("click",function(event){
+            event.preventDefault();
+            $("html,body").animate({"scrollTop":$(".mod-1").offset().top});
+       });
+
+       $("#J_side-nav-2").on("click",function(event){
+            event.preventDefault();
+            $("html,body").animate({"scrollTop":$(".mod-2").offset().top});
+       });
+
+       $("#J_side-nav-3").on("click",function(event){
+            event.preventDefault();
+            $("html,body").animate({"scrollTop":$(".mod-3").offset().top});
+       });
+       $("#J_side-nav-4").on("click",function(event){
+            event.preventDefault();
+            $("html,body").animate({"scrollTop":$(".mod-4").offset().top});
+       })
+
+
         
     })
