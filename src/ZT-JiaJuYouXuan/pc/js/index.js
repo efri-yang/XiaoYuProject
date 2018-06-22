2460a5c2f438e8d4d4c7b7a9f4b76cc40ce296c7
efri-yang <948061564@qq.com>
2018-06-22 09:33:37 +0800

    c

--- src/ZT-JiaJuYouXuan/pc/js/index.js
+++ src/ZT-JiaJuYouXuan/pc/js/index.js
@@ -42,7 +42,34 @@
 
 
 
-        $("#J_lqwy-btn-1,#J_lqwy-btn-2").on("click",function(event){
+        $("#J_lqwy-btn-1").on("click",function(event){
+            event.preventDefault();
+            layer.open({
+              type: 1,
+              shadeClose: true,
+              title: false, //不显示标题
+              area:"450px",
+              content: $('#J_dialog-lqwj'), //捕获的元素，注意：最好该指定的元素要存放在body最外层，否则可能被其它的相对元素所影响
+              cancel: function(){
+                // layer.msg('捕获就是从页面已经存在的元素上，包裹layer的结构', {time: 5000, icon:6});
+              }
+            });
+        });
+
+         $(".top-pic-txt").on("click",function(event){
+            event.preventDefault();
+            layer.open({
+              type: 1,
+              shadeClose: true,
+              title: false, //不显示标题
+              area:"450px",
+              content: $('#J_dialog-lqwj'), //捕获的元素，注意：最好该指定的元素要存放在body最外层，否则可能被其它的相对元素所影响
+              cancel: function(){
+                // layer.msg('捕获就是从页面已经存在的元素上，包裹layer的结构', {time: 5000, icon:6});
+              }
+            });
+        });
+         $(".ft-fixed .top-pic").on("click",function(event){
             event.preventDefault();
             layer.open({
               type: 1,
@@ -101,7 +128,7 @@
         });
 
         //预约成功以后弹出框，开发根据需要嵌入
-        layer.open({
+        var layerResult=layer.open({
               type: 1,
               shadeClose: true,
               title: false, //不显示标题
@@ -111,6 +138,9 @@
                   
               }
          });
+        $("#J_dialog-result .btn-close").on("click",function(){
+            layer.close(layerResult)
+        });
 
 
        $("#J_side-nav-1").on("click",function(event){
@@ -130,7 +160,30 @@
        $("#J_side-nav-4").on("click",function(event){
             event.preventDefault();
             $("html,body").animate({"scrollTop":$(".mod-4").offset().top});
-       })
+       });
+
+
+       $(".dialog-form-box .item-cell .lab").on("click",function(){
+            var $this=$(this);
+            $this.hide();
+            $this.siblings(".ipt-text").focus();
+       });
+
+       $(".dialog-form-box .item-cell .ipt-text").on("foucs",function(){
+            var $this=$(this);
+            $this.hide();
+            $this.siblings(".ipt-text").focus();
+       }).on("blur",function(){
+             var $this=$(this);
+             var val=$.trim($this.val());
+             if(!val){
+                $this.siblings('label.lab').show();
+             }
+       });
+
+
+
+
 
 
         
